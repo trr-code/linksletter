edfcb3261d05d4994c597daa5cf0dd4c07554992
trr-code <154942624+trr-code@users.noreply.github.com>
2025-02-11 11:07:06 -0600

    Links CRUD - resolves #1

---
+++ app/Http/Controllers/LinkController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreLinkRequest;
+use App\Http\Requests\UpdateLinkRequest;
+use App\Models\Link;
+use App\Models\User;
+
+class LinkController extends Controller
+{
+    public function index()
+    {
+        $links = Link::query()
+            // Filter by tenant id (user_id)
+            ->where('user_id', auth()->id())
+            ->orderBy('id', 'desc')
+            ->paginate(50);
+
+        return view('links.index', [
+            'links' => $links,
+        ]);
+    }
+
+    public function create()
+    {
+        $users = User::all();
+
+        return view('links.create', [
+            'users' => $users,
+        ]);
+    }
+
+    public function store(StoreLinkRequest $request)
+    {
+        $link = Link::create(
+            $request->validated() + [
+                'user_id' => auth()->id(),
+            ]
+        );
+
+        // If there is no position, set it to the last
+        if (!$link->position) {
+            $link->position = Link::max('position') + 1;
+            $link->save();
+        }
+
+        return redirect()->route('links.index')
+            ->with('message', 'Link created successfully.');
+    }
+
+    public function edit(Link $link)
+    {
+        // Check if user is the owner of the link
+        abort_unless($link->user_id === auth()->id(), 404);
+
+        $users = User::all();
+
+        return view('links.edit', [
+            'link' => $link,
+            'users' => $users,
+        ]);
+    }
+
+    public function update(UpdateLinkRequest $request, Link $link)
+    {
+        abort_unless($link->user_id === auth()->id(), 404);
+
+        $link->update($request->validated());
+
+        return redirect()->route('links.index')
+            ->with('message', 'Link updated successfully.');
+    }
+
+    public function destroy(Link $link)
+    {
+        abort_unless($link->user_id === auth()->id(), 404);
+
+        $link->delete();
+
+        return redirect()->route('links.index')
+            ->with('message', 'Link deleted successfully.');
+    }
+}
--- database/factories/LinkFactory.php
+++ database/factories/LinkFactory.php
@@ -2,13 +2,18 @@
 
 namespace Database\Factories;
 
+use App\Models\Link;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Link>
  */
 class LinkFactory extends Factory
 {
+    protected $model = Link::class;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +22,16 @@ class LinkFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'url' => $this->faker->url(),
+            'title' => $this->faker->word(),
+            'description' => $this->faker->text(),
+            'position' => $this->faker->randomNumber(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+
+            // Disabled for now, since we don't have issues workflow yet
+            // 'issue_id' => Issue::factory(),
+            'user_id' => User::factory(),
         ];
     }
 }
