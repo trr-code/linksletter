2e817999bf9f2c72972fd5f132302cddf9d3feff
trr-code <154942624+trr-code@users.noreply.github.com>
2025-02-12 16:07:55 -0600

    Adding Larastan Workflow - resolves #9

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -29,7 +29,7 @@ test('email can be verified', function () {
 
     Event::assertDispatched(Verified::class);
     expect($user->fresh()->hasVerifiedEmail())->toBeTrue();
-    $response->assertRedirect(route('dashboard', absolute: false) . '?verified=1');
+    $response->assertRedirect(route('dashboard', absolute: false).'?verified=1');
 })->skip(! new User instanceof MustVerifyEmail);
 
 test('email is not verified with invalid hash', function () {
